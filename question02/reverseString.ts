4483eec071fd4df3f0b64fe40a4576f1f050f27f
chiilog <purgexpurge@gmail.com>
2025-03-13 22:14:00 +0900

    文字列を逆順にする関数を実装し、JSDocコメントを追加

--- question02/reverseString.ts
+++ question02/reverseString.ts
@@ -1,1 +1,8 @@
-export const reverseString = () => {}
+/**
+ * 文字列を逆順にする関数
+ * @param {string} str - 逆順にする文字列
+ * @return {string} 逆順にした文字列
+ */
+export const reverseString = (str: string): string => {
+  return str.split('').reverse().join('');
+}
